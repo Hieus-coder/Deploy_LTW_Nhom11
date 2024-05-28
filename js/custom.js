772319db7fd2b2b20015be1b45832b26daf740be
Hieus-coder <vanhieu1524@gmail.com>
2024-05-28 16:38:31 +0700

    Up deploy

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+var totalAmount = 27;
+var quantity = 1;
+
+var decrementButton = document.getElementById('decrementButton');
+var incrementButton = document.getElementById('incrementButton');
+var quantityDisplay = document.getElementById('quantityDisplay');
+var totalAmountDisplay = document.getElementById('totalAmountDisplay');
+var clearSelectionButton = document.querySelector('.clear_choose');
+var addToCartButton = document.querySelector('.end');
+var deliveryInput = document.getElementById('delivery');
+
+incrementButton.addEventListener('click', function () {
+    quantity++;
+    quantityDisplay.textContent = quantity;
+
+    totalAmount += 27;
+    totalAmountDisplay.textContent = '$' + totalAmount;
+});
+
+decrementButton.addEventListener('click', function () {
+    if (quantity > 0) {
+        quantity--;
+        totalAmount -= 27;
+        quantityDisplay.textContent = quantity;
+        totalAmountDisplay.textContent = '$' + totalAmount;
+    }
+});
+
+clearSelectionButton.addEventListener('click', function () {
+    var checkboxes = document.querySelectorAll('input[type="radio"]');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = false;
+    });
+    deliveryInput.value = '';
+
+    quantity = 1;
+    totalAmount = 27;
+    quantityDisplay.textContent = quantity;
+    totalAmountDisplay.textContent = '$' + totalAmount;
+});
+
+addToCartButton.addEventListener('click', function () {
+    if (deliveryInput.value.trim() === '') {
+        alert('Please enter delivery information.');
+    } else {
+        alert('Your order has been received!');
+    }
+});
