283bd7575d857c5f9e3d336327b43367a4aeb439
Hieus-coder <vanhieu1524@gmail.com>
2024-05-29 08:48:30 +0700

    up

--- js/Home.js
+++ js/Home.js
@@ -50,4 +50,4 @@ const slideContainer = document.getElementById('slide-container');
             }
         }
 
-        setInterval(showNextSlide, 2500);
+        setInterval(showNextSlide, 2500); 
